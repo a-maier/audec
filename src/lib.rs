4d62a661048ad9ee0febb3cc2567947ca71fbafa
Andreas Maier <andreas.martin.maier@desy.de>
2023-12-22 10:37:53 +0100

    new function to guess the compression format

--- src/lib.rs
+++ src/lib.rs
@@ -45,40 +45,75 @@ compile_error!("feature \"lz4\" and feature \"lz4_flex\" cannot be enabled at th
 /// bytes are not recognized or not enough bytes can be read to
 /// determine the format.
 pub fn auto_decompress<'a, B: 'a + BufRead>(mut r: B) -> Box<dyn BufRead + 'a> {
-    let bytes = match r.fill_buf() {
-        Ok(bytes) => bytes,
-        Err(_) => return Box::new(r),
+    use CompressionFormat::*;
+
+    let Some(format) = guess_compression_format(&mut r) else {
+        return Box::new(r)
     };
-    match bytes {
+    match format {
         #[cfg(feature = "bzip2")]
-        [b'B', b'Z', b'h', ..] => {
+        Bzip2 => {
             debug!("Decompress as bzip2");
             Box::new(BufReader::new(bzip2::read::BzDecoder::new(r)))
         },
         #[cfg(feature = "flate2")]
-        &[0x1f, 0x8b, ..] => {
-            debug!("Decompress as gzip");
+        Deflate => {
+            debug!("Decompress as Deflate");
             Box::new(BufReader::new(flate2::read::GzDecoder::new(r)))
         },
         #[cfg(feature = "lz4")]
-        &[0x04, 0x22, 0x4d, 0x18, ..] => {
+        Lz4 => {
             debug!("Decompress as lz4");
             Box::new(BufReader::new(lz4::Decoder::new(r).unwrap()))
         },
         #[cfg(feature = "lz4_flex")]
-        &[0x04, 0x22, 0x4d, 0x18, ..] => {
+        Lz4 => {
             debug!("Decompress as lz4");
             Box::new(BufReader::new(lz4_flex::frame::FrameDecoder::new(r)))
         },
         #[cfg(feature = "zstd")]
-        &[0x28, 0xb5, 0x2f, 0xfd, ..] => {
+        Zstd => {
             debug!("Decompress as zstd");
             Box::new(BufReader::new(zstd::stream::Decoder::new(r).unwrap()))
         },
+        #[allow(unreachable_patterns)]
         _ => {
             debug!("No decompression");
             Box::new(r)
         }
+    }
+}
+
+/// Compression format
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum CompressionFormat {
+    /// The [Deflate](https://en.wikipedia.org/wiki/Deflate) format
+    Deflate,
+    /// The [bzip2](https://en.wikipedia.org/wiki/bzip2) format
+    Bzip2,
+    /// The [LZ4](https://en.wikipedia.org/wiki/LZ4_(compression_algorithm)) format
+    Lz4,
+    /// The [Zstandard](https://en.wikipedia.org/wiki/Zstd) format
+    Zstd,
+}
+
+/// Guess the compression format
+///
+/// The format is determined by looking at the leading magic
+/// bytes. Returns `None` if the magic bytes are not recognized or not
+/// enough bytes can be read to determine the format.
+pub fn guess_compression_format<B: BufRead>(r: &mut B) -> Option<CompressionFormat> {
+    use CompressionFormat::*;
+
+    let Ok(bytes) = r.fill_buf() else {
+        return None;
+    };
+    match bytes {
+        [b'B', b'Z', b'h', ..] => Some(Bzip2),
+        &[0x1f, 0x8b, ..] => Some(Deflate),
+        &[0x04, 0x22, 0x4d, 0x18, ..] => Some(Lz4),
+        &[0x28, 0xb5, 0x2f, 0xfd, ..] => Some(Zstd),
+        _ => None
     }
 }
 
